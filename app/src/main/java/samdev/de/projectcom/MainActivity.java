6ce29e1d651f99b0a383af77499a5ba219b53c4c
Anastron <benz.softwaredeveloping@gmail.com>
2015-12-11 19:26:36 +0100

    #more things about navigation drawer #new Activity for adding Player

--- app/src/main/java/samdev/de/projectcom/MainActivity.java
+++ app/src/main/java/samdev/de/projectcom/MainActivity.java
@@ -1,8 +1,12 @@
 package samdev.de.projectcom;
 
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.Configuration;
+import android.preference.PreferenceManager;
 import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -11,14 +15,19 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import samdev.de.projectcom.activitys.PlayerActivity;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements NavigationView.OnNavigationItemSelectedListener {
 
+    private static final String SELECTED_ITEM_ID = "selected";
+    private static final String FIRST_TIME = "first_time";
     private Toolbar toolbar;
     private NavigationView mDrawer;
     private DrawerLayout mDrawerlayout;
     ActionBarDrawerToggle mDrawerToggle;
+    private int mSelectedId;
+    private boolean mUserSawDrawer = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,13 +39,28 @@ public class MainActivity extends ActionBarActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
         mDrawer = (NavigationView) findViewById(R.id.main_drawer);
+        mDrawer.setNavigationItemSelectedListener(this);
         mDrawerlayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerlayout, toolbar, R.string.drawer_open, R.string.drawer_close);
 
         mDrawerlayout.setDrawerListener(mDrawerToggle);
         mDrawerToggle.syncState();
 
+        if(!didUserSeeDrawer()){
+            showDrawer();
+            markDrawerSeen();
+        }
+        else
+        {
+            hideDrawer();
+        }
+
+        //mSelectedId = savedInstanceState == null ? R.id.navigation_item_1 : savedInstanceState.getInt(SELECTED_ITEM_ID);
+        // lassen wir es vorerst immer auf dem Hauptscreen sein
+        navigate(mSelectedId);
+
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -63,5 +87,63 @@ public class MainActivity extends ActionBarActivity {
     public void onConfigurationChanged(Configuration newConfig){
         super.onConfigurationChanged(newConfig);
         mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    private boolean didUserSeeDrawer(){
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        mUserSawDrawer = sharedPreferences.getBoolean(FIRST_TIME, false);
+        return mUserSawDrawer;
+    }
+
+    private void showDrawer(){
+        mDrawerlayout.openDrawer(GravityCompat.START);
+    }
+
+    private void hideDrawer(){
+        mDrawerlayout.closeDrawer(GravityCompat.START);
+    }
+
+    private void markDrawerSeen(){
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        mUserSawDrawer = true;
+        sharedPreferences.edit().putBoolean(FIRST_TIME, mUserSawDrawer).apply();
+    }
+
+    public void navigate(int mSelectedId){
+        Intent intent = null;
+        if(mSelectedId == R.id.navigation_item_1)
+        {
+            mDrawerlayout.closeDrawer(GravityCompat.START);
+            intent = new Intent(this, PlayerActivity.class);
+            startActivity(intent);
+        }
+        if(mSelectedId == R.id.navigation_item_2)
+        {
+
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+
+        item.setChecked(true);
+        mSelectedId = item.getItemId();
+
+        navigate(mSelectedId);
+        return true;
+    }
+
+    protected void onSaveInstanceState(Bundle outState){
+        super.onSaveInstanceState(outState);
+        outState.putInt(SELECTED_ITEM_ID, mSelectedId);
+    }
+
+    public void onBackPressed(){
+        if(mDrawerlayout.isDrawerOpen(GravityCompat.START)){
+            mDrawerlayout.closeDrawer(GravityCompat.START);
+        }
+        else{
+            super.onBackPressed();
+        }
     }
 }
---
+++ app/src/main/java/samdev/de/projectcom/activitys/PlayerActivity.java
@@ -0,0 +1,65 @@
+package samdev.de.projectcom.activitys;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.preference.PreferenceManager;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.NavUtils;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import samdev.de.projectcom.R;
+
+public class PlayerActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player);
+
+        toolbar = (Toolbar)findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_player, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        if(id == android.R.id.home){
+            NavUtils.navigateUpFromSameTask(this);
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
